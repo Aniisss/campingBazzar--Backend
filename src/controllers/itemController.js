a4b3b5279fa8552dcd1a135c35afcf6af34e26bc
Aniisss <anisnsir74@gmail.com>
2024-12-17 17:13:54 +0100

    adding createItem api

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -1,25 +1,42 @@
-const itemSchema = require('../models/item')
+const { db, admin } = require('../config/firebase_adminSDK_config');
+const itemSchema = require('../models/item'); 
+const { GeoFirestore } = require('geofirestore'); 
+const { Timestamp } = require('firebase-admin').firestore;
+
+const geo = new GeoFirestore(db);
+const geoFirestore = geo.collection("items");
 
 
 // Create the article function
 async function createItem(req, res) {
     try {
+
+        //extracting the location and deleting from request to validate without location
+        const location = req.body.location ; 
+        delete req.body.location;
+
         // Validate request body
         const { error, value} = itemSchema.validate(req.body, { abortEarly: true });
         if (error) {
             return res.status(400).json({ error: error.details[0].message });
         }
 
-        // Add the article to Firestore
-        const docRef = firestore().collection('items').doc();
-        const data = {
-            itemID : docRef.id,
-            ...value 
+        const itemRef = db.collection("items").doc();
+        const itemID = itemRef.id;
 
-        }
-        await docRef.set(object);
+        // Create the article document with geospatial data
+        await geoFirestore.doc(itemID).set({
+          ...value,
+          itemID : itemID ,
+          createdAt : Timestamp.now(),
+          coordinates: new admin.firestore.GeoPoint(
+            location.latitude,
+            location.longitude
+          ),
+        });
 
-        return res.status(201).json({ message: 'Article created successfully', articleID:  docRef.id});
+
+        return res.status(201).json({ message: 'Item created successfully', itemID:  itemID});
     } catch (err) {
         console.error('Error creating article:', err);
         return res.status(500).json({ error: 'Internal Server Error' });
--- src/models/item.js
+++ src/models/item.js
@@ -3,10 +3,9 @@ const joi = require('joi');
 const itemSchema = joi.object({
     title : joi.string() , 
     description : joi.string().default('') ,
-    image : joi.string() ,
-    price : joi.number() ,
+    image : joi.string().default('') ,
+    price : joi.number().default(0) ,
     category : joi.string() ,
-    isNew : joi.boolean() ,
 })
 
 module.exports = itemSchema ;
