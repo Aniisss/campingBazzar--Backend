0a8458b40ff50592ebc2201d44b45925203b3791
Aniisss <anisnsir74@gmail.com>
2024-12-18 14:45:57 +0100

    add upload endpoint for item creation

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -2,9 +2,17 @@ const { db, admin } = require('../config/firebase_adminSDK_config');
 const itemSchema = require('../models/item'); 
 const { GeoFirestore } = require('geofirestore'); 
 const { Timestamp } = require('firebase-admin').firestore;
+const { BlobServiceClient } = require('@azure/storage-blob');
 
 const geo = new GeoFirestore(db);
 const geoFirestore = geo.collection("items");
+
+// Azure Blob Storage setup
+const AZURE_CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=campigngbazzar;AccountKey=MfwXKgK0CY6hnxQ5LbVNW/kSvDBHUvHfvZQmz3qohcaZibSNESJcIYo5JUCdztR4uory1k8tC3Rz+AStjmjodg==;EndpointSuffix=core.windows.net';
+const containerName = 'items';
+
+const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_CONNECTION_STRING);
+const containerClient = blobServiceClient.getContainerClient(containerName);
 
 
 // Create the article function
@@ -22,6 +30,7 @@ async function createItem(req, res) {
         // Validate request body
         const { error, value} = itemSchema.validate(req.body, { abortEarly: true });
         if (error) {
+            console.log(error);
             return res.status(400).json({ error: error.details[0].message });
         }
 
@@ -48,4 +57,33 @@ async function createItem(req, res) {
     }
 };
 
-module.exports = createItem;
+async function uploadImage(req, res) {
+  try {
+    const filePath = req.file.path;
+
+    // Generate a unique blob name by appending a timestamp to the original file name
+    const timestamp = Date.now(); // Get the current timestamp in milliseconds
+    const blobName = `${timestamp}_${req.file.originalname}`;
+
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    
+    // Upload the image to Azure Blob Storage
+    const uploadBlobResponse = await blockBlobClient.uploadFile(filePath);
+
+    // Generate the URL for the uploaded image
+    const imageUrl = `https://${blobServiceClient.accountName}.blob.core.windows.net/${containerName}/${blobName}`;
+
+    // Return success response with image URL
+    res.status(200).json({
+      message: "Image uploaded successfully!",
+      imageUrl: imageUrl
+    });
+
+  } catch (error) {
+    console.error('Error uploading to Azure:', error);
+    res.status(500).json({ error: 'Failed to upload image' });
+  }
+}
+
+
+module.exports = {createItem , uploadImage};
--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const authenticate = require('../middlewares/auth');
-const createItem = require('../controllers/itemController');
+const {createItem , uploadImage} = require('../controllers/itemController');
+
+
+// for uploading images
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' }); // Temporary storage
 
 
 
 // Route for signing in a user
 router.post('/createItem', authenticate, createItem);
+router.post('/upload' , upload.single('image'),authenticate , uploadImage)
 
 module.exports = router;
